ee1d6da88b54f2629aa879c350a9db24db1f4483
Ofir <ofir.duchovne@gmail.com>
2021-11-23 15:17:07 +0200

    authonticaiton Functions

---
+++ modules/authenticator.js
@@ -0,0 +1,38 @@
+const DB = require("../data/index");
+
+const Tokens = {};
+
+function createToken() {
+    const token = Math.random().toString(36).substring(2);
+    Tokens[token] = new Date(Date.now() + 600000);
+    return token;
+}
+
+function validateToken(token) {
+    if (Tokens[token] && Tokens[token] > Date.now()) {
+        return true;
+    }
+    if (Tokens[token]) {
+        removeToken(token);
+        throw new Error("Token expired");
+    } else {
+        throw new Error("Invalid token"); 
+    }
+}
+
+function removeToken(token) {
+    delete Tokens[token];
+}
+
+function giveToken(user) {
+    if(DB.isUserRegistered(user)) {
+        return createToken();
+    }
+    throw new Error("User not registered");
+}
+
+
+module.exports = {
+    giveToken,
+    validateToken
+};
